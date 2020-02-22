8fcd0009a0f5ebda0948f15166c15da2a8e10f1c
bxcar <malanchukdima@mail.ru>
2020-02-22 05:10:29 +0200

    some changes

--- app/js/main-gpl.js
+++ app/js/main-gpl.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function () {
+$(document).ready(function () {
     document.querySelectorAll('.header__menu-item').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -7,5 +7,23 @@ jQuery(document).ready(function () {
                 behavior: 'smooth'
             });
         });
+    });
+
+    $( ".section1__titles a" ).click(function(e) {
+        e.preventDefault();
+        $(".section1__titles a").removeClass('active');
+        $(".section1__titles2 a").removeClass('active');
+        $(".section1__logos-inner-wrapper").removeClass('active');
+        $(this).addClass('active');
+        $(".section1__logos-inner-wrapper[data-title='" + $(this).attr('data-title') + "']").addClass('active');
+    });
+
+    $( ".section1__titles2 a" ).click(function(e) {
+        e.preventDefault();
+        $(".section1__titles a").removeClass('active');
+        $(".section1__titles2 a").removeClass('active');
+        $(".section1__logos-inner-wrapper").removeClass('active');
+        $(this).addClass('active');
+        $(".section1__logos-inner-wrapper[data-title='" + $(this).attr('data-title') + "']").addClass('active');
     });
 });
